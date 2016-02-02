261cc1c0131ceed2cbe397fda0d7e96422516277
saepudin <saepudin.0708@gmail.com>
2016-02-02 14:24:48 +0700

    origin

--- app/routing.php
+++ app/routing.php
@@ -2,3 +2,6 @@
 
 $app->get('/', 'Pamit\Controllers\HomeController:index')
     ->setName('home');
+
+$app->get('/admin', 'Pamit\Controllers\AdminHomeController:index')
+    ->setName('admin');
---
+++ src/Controllers/AdminHomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pamit\Controllers;
+
+use Pamit\Core\Controller;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+/**
+* Admin
+*/
+class AdminHomeController extends Controller
+{
+    public function index(Request $request, Response $response, $args)
+    {
+
+        return $this->view->render($response, 'adminhome.twig');
+    }
+}
